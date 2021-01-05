89bf40bcc913a415d7420a264c2f71a08455013d
navkc11 <63931144+navkc11@users.noreply.github.com>
2021-01-05 23:34:16 +0200

    Add files via upload

---
+++ Library.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Library {
+    ArrayList<Book> books;
+    ArrayList<Rental> rental;
+    //banai
+    public Library(){
+        books = new ArrayList<>();
+        rental = new ArrayList<>();
+    }
+    //method 1
+//    public Author mostPopular(){
+//        for (Book most:books) {
+//
+//        }
+//        books.get()
+//    }
+}
